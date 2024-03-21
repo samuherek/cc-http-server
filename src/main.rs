cad12a4194d0143797aac391245aa054efb749bd
Sam Uherek <samuherekbiz@gmail.com>
2024-03-21 18:39:10 +0100

    impl response with content

--- src/main.rs
+++ src/main.rs
@@ -12,18 +12,28 @@ fn main() -> anyhow::Result<()> {
                 let mut buffer = [0; 1024];
                 stream.read(&mut buffer).context("read stream into data")?;
                 let request = String::from_utf8_lossy(&buffer[..]);
-                if let Some(line) = request.lines().next() {
-                    match line.split_whitespace().take(2).last() {
-                        Some("/") => {
-                            stream
-                                .write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
-                                .context("write response to stream")?;
-                        }
-                        _ => {
-                            stream
-                                .write_all("HTTP/1.1 404 Not Found\r\n\r\n".as_bytes())
-                                .context("write response to stream")?;
-                        }
+                let path = request
+                    .lines()
+                    .next()
+                    .unwrap_or_default()
+                    .split_whitespace()
+                    .take(2)
+                    .last();
+
+                if let Some(path) = path {
+                    if path.starts_with("/echo/") {
+                        let res = path.strip_prefix("/echo/").unwrap_or_default();
+                        let response = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", res.len(), res);
+                        stream.write_all(response.as_bytes()).context("write response to stream with echo")?;
+
+                    } else if path == "/" {
+                        stream
+                            .write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
+                            .context("write response to stream")?;
+                    } else {
+                        stream
+                            .write_all("HTTP/1.1 404 Not Found\r\n\r\n".as_bytes())
+                            .context("write response to stream")?;
                     }
                 }
             }
